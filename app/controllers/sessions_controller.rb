cd5e153ed7590f715923657c2489354c0a25e026
Sabastian <sswbelser@gmail.com>
2015-09-04 12:18:35 -0700

    Another redirect fix attempt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,17 @@ class SessionsController < ApplicationController
     if user && user.authenticate(user_params[:password])
       session[:user_id] = user.id
       flash[:notice] = "Successfully logged in!"
-      redirect_to root_page
+      redirect_to root_path
     else
       flash[:error] = "Incorrect email or password. Please try again."
-      redirect_to root_page
+      redirect_to root_path
     end
   end
 
   def destroy
     session[:user_id] = nil
     flash[:notice] = "Successfully logged out!"
+    redirect_to root_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
 
+
 # add new user to database
   def create
       user = User.new(user_params)
       if user.save
         session[:user_id] = user.id
         flash[:notice] = "Successfully signed up!"
-        redirect_to root_page
+        redirect_to root_path
       else
         flash[:error] = user.errors.full_messages.join(', ')
-        redirect_to root_page
+        redirect_to root_path
       end
   end
 
