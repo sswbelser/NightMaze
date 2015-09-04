e8bf728a94436aa8f290eaa7e82b3cca9b5ef537
Sabastian <sswbelser@gmail.com>
2015-09-04 12:41:43 -0700

    Added jQuery Validate, removed edit user routes

---
+++ app/assets/javascripts/validation.js
@@ -0,0 +1,92 @@
+// jQuery Validate
+$(function() {
+
+	$("#new_user").validate({
+		rules: {
+			"user[username]": {
+				required: true,
+				minlength: 2
+			},
+			"user[email]": {
+				required: true,
+				email: true,
+			},
+			"user[password]": {
+				required: true,
+				minlength: 6
+			}
+		},
+		messages: {
+			"user[username]": {
+				required: "Please enter a username",
+				minlength: "Username must be longer than 2 characters"
+			},
+			"user[email]": {
+				required: "Please enter an email",
+				email: "Please enter a valid email"
+			},
+			"user[password]": {
+				required: "Please enter a password",
+				minlength: "Your password must be at least 6 characters"
+			}
+		}
+	});
+
+	$("#login_form").validate({
+		rules: {
+			"user[email]": {
+				required: true,
+				email: true
+			},
+			"user[password]": {
+				required: true,
+				minlength: 6
+			}
+		},
+		messages: {
+			"user[email]": {
+				required: "Please enter an email",
+				email: "Please enter a valid email"
+			},
+			"user[password]": {
+				required: "Please enter a password",
+				minlength: "Your password must be at least 6 characters"
+			},
+		}
+	});
+
+	// $("#new-post").validate({
+	// 	rules: {
+	// 		"post[body]": {
+	// 			required: true,
+	// 			minlength: 2,
+	// 			maxlength: 200
+	// 		}
+	// 	},
+	// 	messages: {
+	// 		"post[body]": {
+	// 			required: "Please enter a post",
+	// 			minlength: "Your post must be at least 2 characters",
+	// 			maxlength: "Your post must be shorter than 200 characters"
+	// 		}
+	// 	}
+	// });
+
+	// $("#edit-post").validate({
+	// 	rules: {
+	// 		"post[body]": {
+	// 			required: true,
+	// 			minlength: 2,
+	// 			maxlength: 200
+	// 		}
+	// 	},
+	// 	messages: {
+	// 		"post[body]": {
+	// 			required: "Please enter a post",
+	// 			minlength: "Your post must be at least 2 characters",
+	// 			maxlength: "Your post must be shorter than 200 characters"
+	// 		}
+	// 	}
+	// });
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
 	get "/about", to: "pages#show", as: :about
 
-	resources :users, only: [:create, :update, :destroy]
-	get "/edit-user", to: "users#edit", as: :edit_user
+	resources :users, only: [:create, :destroy]
 
 	resources :sessions, only: [:create]
 	get "/logout", to: "sessions#destroy", as: :logout
